618da0d6870858186775a4a09cceb972cd0b96c3
cristianetaniguti <chaytaniguti@gmail.com>
2022-04-23 16:02:00 -0500

    tests by software

--- tests/testthat/test-genes.R
+++
@@ -1,1 +0,0 @@
-# Gene annotation graphic and tables
--- tests/testthat/test-tetra_example.R
+++ tests/testthat/test-tetra_example.R
@@ -1,7 +1,59 @@
-test_that("QTL graphics and tables",{
-  
+test_that("upload files",{
+  # upload examples
   viewpoly_obj <- prepare_examples("tetra_map")
   
+  expect_equal(check_viewpoly(viewpoly_obj),0)
+  
+  check_viewmap_values(viewpoly_obj$map, 
+                       c(14, 132, 139, 157, 34), 
+                       c(36, 167, 164, 109), 
+                       50502.07)
+  
+  check_viewqtl_qtlpoly_values(viewpoly_obj$qtl, 
+                               116504, 
+                               5.418909, 
+                               -1.067457e-11, 
+                               299.6155, 
+                               0.000160791, 
+                               2.340129e-12, 
+                               1)
+  
+  # VIEWmap tests 
+  qtl_profile_plot <- plot_profile(viewpoly_obj$qtl$profile, 
+                                   viewpoly_obj$qtl$qtl_info, 
+                                   viewpoly_obj$qtl$selected_mks, 
+                                   pheno.col = 2, 
+                                   lgs.id = 2, 
+                                   by_range = TRUE, 
+                                   range.min = 30, 
+                                   range.max = 120, 
+                                   plot=TRUE, 
+                                   software = NULL)
+  
+  expect_equal(sum(qtl_profile_plot$data$SIG, na.rm = TRUE), 43.81917, tolerance = 0.0001)
+  
+  maps <- lapply(viewpoly_obj$map$maps, function(x) {
+    y <- x$l.dist
+    names(y) <- x$mk.names
+    y
+  })
+  
+  # Get max size each chromosome
+  expect_equal(map_summary(left.lim = 1, 
+                           right.lim = 50, 
+                           ch = 3, 
+                           maps = maps, 
+                           d.p1 = viewpoly_obj$map$d.p1, 
+                           d.p2 = viewpoly_obj$map$d.p2)[[5]], 134.073, tolerance = 0.0001)
+  
+  # Map summary table
+  summary_table <- summary_maps(viewpoly_obj$map)
+  expect_equal(sum(as.numeric(summary_table$`Map length (cM)`)), 3259.98)
+  expect_equal(sum(as.numeric(summary_table$Simplex)), 2450)
+  expect_equal(sum(as.numeric(summary_table$`Double-simplex`)), 1820)
+  expect_equal(sum(as.numeric(summary_table$`Max gap`)), 80.51)
+  
+  #VIEWqtl tests
   # by range
   qtl_profile_data <- plot_profile(viewpoly_obj$qtl$profile, 
                                    viewpoly_obj$qtl$qtl_info, 
@@ -111,7 +163,7 @@ test_that("QTL graphics and tables",{
                                 1:5)
   
   expect_equal(sum(data.prob$homoprob$probability), 14900, tolerance = 0.001)
-
+  
   input.haplo <- c("Trait:SG06_LG:2_Pos:77_homolog:P1.1")
   p <- select_haplo(input.haplo, 
                     viewpoly_obj$qtl$probs, 
@@ -120,4 +172,11 @@ test_that("QTL graphics and tables",{
   expect_equal(sum(p[[1]]$data$probability), 507.9996, tolerance = 0.0001)
   expect_equal(sum(p[[2]]$data$probability), 508.001, tolerance = 0.0001)
   expect_equal(sum(p[[3]]$data$probability), 508.0009, tolerance = 0.0001)
+  
+  # VIEWgenome tests
+  p <- plot_cm_mb(viewpoly_obj$map, 1, 1,50)
+  
+  expect_equal(sum(p$data$l.dist), 50502.07, tolerance = 0.001)
+  
 })
+
