fa80297c616a349d5ecbc729b342d678a1d91076
cristianetaniguti <chaytaniguti@gmail.com>
2022-04-25 12:35:23 -0500

    fix path 2

--- tests/testthat/test-shinyTest.R
+++ tests/testthat/test-shinyTest.R
@@ -4,6 +4,5 @@ test_that("shinytest", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
   
-  appdir <- gsub("inst", "", system.file(package = "viewpoly"))
-  expect_pass(testApp(appDir = appdir, testnames = "shinyTest_all_tabs", compareImages = FALSE))
+  expect_pass(testApp(testnames = "shinyTest_all_tabs", compareImages = FALSE))
 })
