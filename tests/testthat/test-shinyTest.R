d42bee1e3da770a0b4c931253d35d1201692d822
cristianetaniguti <chaytaniguti@gmail.com>
2022-04-25 14:54:55 -0500

    add in rsconnect #29

--- tests/testthat/test-shinyTest.R
+++ tests/testthat/test-shinyTest.R
@@ -4,6 +4,5 @@ test_that("shinytest", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
   
-  appdir <- gsub("inst", "", system.file(package = "viewpoly"))
-  expect_pass(testApp(appDir = appdir,compareImages = FALSE))
+  expect_pass(testApp(compareImages = FALSE))
 })
