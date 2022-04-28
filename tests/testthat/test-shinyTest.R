b88203a1d74a60d53b57498748104a3ccaa52ac2
cristianetaniguti <chaytaniguti@gmail.com>
2022-04-28 16:23:35 -0500

    change path

--- tests/testthat/test-shinyTest.R
+++ tests/testthat/test-shinyTest.R
@@ -3,10 +3,6 @@ library(shinytest)
 test_that("shinytest", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
-  
-  if(!grepl("inst", system.file(package = "viewpoly"))) 
-    appdir <- paste0(system.file(package = "viewpoly"), "/inst")
-  else appdir <-  system.file(package = "viewpoly")
-  
-  expect_pass(testApp(appDir = appdir, testnames = c("github_actions_tests"),compareImages = FALSE))
+
+  expect_pass(testApp(appDir = system.file(package = "viewpoly"), testnames = c("github_actions_tests"),compareImages = FALSE))
 })
