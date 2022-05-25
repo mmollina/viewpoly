1e74cc8d1b19d0f3d68aab812f303b1041cddeba
cristianetaniguti <chaytaniguti@gmail.com>
2022-05-25 10:35:06 -0500

    fix check

--- tests/testthat/test-shinyTest.R
+++ tests/testthat/test-shinyTest.R
@@ -4,5 +4,5 @@ test_that("shinytest", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
 
-  expect_pass(testApp(appDir = system.file(package = "viewpoly"), testnames = c("github_actions_tests"),compareImages = FALSE))
+  expect_pass(testApp(appDir = gsub("inst", "", system.file(package = "viewpoly")), testnames = c("github_actions_tests"),compareImages = FALSE))
 })
