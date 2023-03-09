54662da9fa32a129c721ac15dfaf2a4317caa873
cristianetaniguti <chaytaniguti@gmail.com>
2023-03-09 14:50:29 -0600

    fix test, update README

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -731,8 +731,11 @@ globalVariables("js")
 
 # Check internet connection
 havingIP <- function() {
-  ipmessage <- system("ipconfig", intern = TRUE)
+  if (.Platform$OS.type == "windows") {
+    ipmessage <- system("ipconfig", intern = TRUE)
+  } else {
+    ipmessage <- system("ifconfig", intern = TRUE)
+  }
   validIP <- "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.]){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
   any(grep(validIP, ipmessage))
 }
-
