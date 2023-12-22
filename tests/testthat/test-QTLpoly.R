69c55bcdeeda80af5de50ffc264846f3dd5a66cf
cristianetaniguti <chaytaniguti@gmail.com>
2023-12-22 11:03:48 -0600

    fix parents names and exclude haplo

--- tests/testthat/test-QTLpoly.R
+++ tests/testthat/test-QTLpoly.R
@@ -1,14 +1,14 @@
 test_that("Tests uploaded QTLpoly files",{
   skip_on_ci() # Large files to be downloaded, continuous integration fails because of download timeout
   source(system.file("ext/functions4tests.R", package = "viewpoly"))
-
+  
   # upload QTLpoly
   input.data <- remim.mod <- est.effects <- fitted.mod <- list()
   input.data$datapath <- tempfile()
   remim.mod$datapath <- tempfile()
   est.effects$datapath <- tempfile()
   fitted.mod$datapath <- tempfile()
-  
+
   if(havingIP()){
     options(timeout=200)
     download.file("https://www.polyploids.org/sites/default/files/2022-04/tetra_QTLpoly_effects.RData", destfile = est.effects$datapath)
@@ -17,9 +17,9 @@ test_that("Tests uploaded QTLpoly files",{
     download.file("https://www.polyploids.org/sites/default/files/2022-04/tetra_QTLpoly_fitted.RData", destfile = fitted.mod$datapath)
     
     viewqtl_qtlpoly <- prepare_QTLpoly(data = input.data,
-                                       remim.mod = remim.mod,
-                                       est.effects = est.effects,
-                                       fitted.mod = fitted.mod)
+                                                  remim.mod = remim.mod,
+                                                  est.effects = est.effects,
+                                                  fitted.mod = fitted.mod)
     
     expect_equal(check_viewqtl(viewqtl_qtlpoly),0)
     
@@ -35,29 +35,29 @@ test_that("Tests uploaded QTLpoly files",{
     #VIEWqtl tests
     # plotly
     qtl_profile_plot <- plot_profile(viewqtl_qtlpoly$profile,
-                                     viewqtl_qtlpoly$qtl_info,
-                                     viewqtl_qtlpoly$selected_mks,
-                                     pheno.col = 2,
-                                     lgs.id = 2,
-                                     by_range = TRUE,
-                                     range.min = 30,
-                                     range.max = 120,
-                                     plot=TRUE,
-                                     software = NULL)
+                                                viewqtl_qtlpoly$qtl_info,
+                                                viewqtl_qtlpoly$selected_mks,
+                                                pheno.col = 2,
+                                                lgs.id = 2,
+                                                by_range = TRUE,
+                                                range.min = 30,
+                                                range.max = 120,
+                                                plot=TRUE,
+                                                software = NULL)
     
     expect_equal(sum(qtl_profile_plot$data$SIG, na.rm = TRUE), 43.81917, tolerance = 0.0001)
     
     # by range
     qtl_profile_data <- plot_profile(viewqtl_qtlpoly$profile,
-                                     viewqtl_qtlpoly$qtl_info,
-                                     viewqtl_qtlpoly$selected_mks,
-                                     pheno.col = 2,
-                                     lgs.id = 2,
-                                     by_range = TRUE,
-                                     range.min = 30,
-                                     range.max = 120,
-                                     plot=FALSE,
-                                     software = NULL)
+                                                viewqtl_qtlpoly$qtl_info,
+                                                viewqtl_qtlpoly$selected_mks,
+                                                pheno.col = 2,
+                                                lgs.id = 2,
+                                                by_range = TRUE,
+                                                range.min = 30,
+                                                range.max = 120,
+                                                plot=FALSE,
+                                                software = NULL)
     
     expect_equal(sum(qtl_profile_data$lines$SIG, na.rm = TRUE), 43.81917, tolerance = 0.001)
     expect_equal(sum(qtl_profile_data$lines$`Position (cM)`), 8000.109, tolerance = 0.001)
@@ -68,15 +68,15 @@ test_that("Tests uploaded QTLpoly files",{
     
     # export data
     qtl_profile_data <- plot_profile(viewqtl_qtlpoly$profile,
-                                     viewqtl_qtlpoly$qtl_info,
-                                     viewqtl_qtlpoly$selected_mks,
-                                     pheno.col = 2,
-                                     lgs.id = 2,
-                                     by_range = FALSE,
-                                     range.min = NULL,
-                                     range.max = NULL,
-                                     plot=FALSE,
-                                     software = NULL)
+                                                viewqtl_qtlpoly$qtl_info,
+                                                viewqtl_qtlpoly$selected_mks,
+                                                pheno.col = 2,
+                                                lgs.id = 2,
+                                                by_range = FALSE,
+                                                range.min = NULL,
+                                                range.max = NULL,
+                                                plot=FALSE,
+                                                software = NULL)
     
     expect_equal(sum(qtl_profile_data$lines$SIG), 292.883, tolerance = 0.001)
     expect_equal(sum(qtl_profile_data$lines$`Position (cM)`), 8000.109, tolerance = 0.001)
@@ -91,13 +91,13 @@ test_that("Tests uploaded QTLpoly files",{
     
     # effects graphics
     p <- data_effects(qtl_info = viewqtl_qtlpoly$qtl_info,
-                      effects = viewqtl_qtlpoly$effects,
-                      pheno.col = "SG06",
-                      lgs = 2,
-                      groups = 2,
-                      position = 77,
-                      software = "QTLpoly",
-                      design = "circle")
+                                 effects = viewqtl_qtlpoly$effects,
+                                 pheno.col = "SG06",
+                                 lgs = 2,
+                                 groups = 2,
+                                 position = 77,
+                                 software = "QTLpoly",
+                                 design = "circle")
     
     expect_equal(sum(p[[1]]$data$Estimates), -0.0436829, tolerance = 0.001)
     expect_equal(names(p[[1]]$data),
@@ -105,13 +105,13 @@ test_that("Tests uploaded QTLpoly files",{
                  tolerance = 0.001)
     
     p <- data_effects(qtl_info = viewqtl_qtlpoly$qtl_info,
-                      effects = viewqtl_qtlpoly$effects,
-                      pheno.col = "SG06",
-                      lgs = 2,
-                      groups = 2,
-                      position = 77,
-                      software = "QTLpoly",
-                      design = "digenic")
+                                 effects = viewqtl_qtlpoly$effects,
+                                 pheno.col = "SG06",
+                                 lgs = 2,
+                                 groups = 2,
+                                 position = 77,
+                                 software = "QTLpoly",
+                                 design = "digenic")
     
     expect_equal(sum(p[[1]]$data$z), 1.528847e-14, tolerance = 0.001)
     expect_equal(names(p[[1]]$data),
@@ -119,13 +119,14 @@ test_that("Tests uploaded QTLpoly files",{
                  tolerance = 0.001)
     
     p <-  data_effects(qtl_info = viewqtl_qtlpoly$qtl_info,
-                       effects = viewqtl_qtlpoly$effects,
-                       pheno.col = "SG06",
-                       lgs = 2,
-                       groups = 2,
-                       position = 77,
-                       software = "QTLpoly",
-                       design = "bar")
+                                  effects = viewqtl_qtlpoly$effects,
+                                  pheno.col = "SG06",
+                                  lgs = 2,
+                                  groups = 2,
+                                  position = 77,
+                                  software = "QTLpoly",
+                                  design = "bar",
+                                  parents = c("doida", "doido"))
     
     expect_equal(sum(p[[1]]$data$Estimates), 2.184058e-15, tolerance = 0.001)
     expect_equal(names(p[[1]]$data),
@@ -135,19 +136,19 @@ test_that("Tests uploaded QTLpoly files",{
     # breeding values table
     pos <- split(viewqtl_qtlpoly$qtl_info[1:3,]$Pos, viewqtl_qtlpoly$qtl_info[1:3,]$pheno)
     breed.values <- breeding_values(viewqtl_qtlpoly$qtl_info,
-                                    viewqtl_qtlpoly$probs,
-                                    viewqtl_qtlpoly$selected_mks,
-                                    viewqtl_qtlpoly$blups,
-                                    viewqtl_qtlpoly$beta.hat,
-                                    pos)
+                                               viewqtl_qtlpoly$probs,
+                                               viewqtl_qtlpoly$selected_mks,
+                                               viewqtl_qtlpoly$blups,
+                                               viewqtl_qtlpoly$beta.hat,
+                                               pos)
     
     expect_equal(sum(breed.values$PY06), 155.63)
     expect_equal(sum(breed.values$SG06), 167.16)
     
     # get and plot homologs prob
     data.prob <- calc_homologprob(probs = viewqtl_qtlpoly$probs,
-                                  viewqtl_qtlpoly$selected_mks,
-                                  1:5)
+                                             viewqtl_qtlpoly$selected_mks,
+                                             1:5)
     
     expect_equal(sum(data.prob$homoprob$probability), 464880, tolerance = 0.001)
     
@@ -155,20 +156,21 @@ test_that("Tests uploaded QTLpoly files",{
                         "Trait:SG06_LG:2_Pos:77_homolog:P1.3", "Trait:FM07_LG:5_Pos:26_homolog:P2.3")
     
     p1.list <- select_haplo(input.haplo,
-                            viewqtl_qtlpoly$probs,
-                            viewqtl_qtlpoly$selected_mks,
-                            effects.data = p)
+                                       viewqtl_qtlpoly$probs,
+                                       viewqtl_qtlpoly$selected_mks,
+                                       effects.data = p)
     p1 <- p1.list[[1]]
     
     # Test exclude
-    input.haplo <- list("Trait:PY06_LG:5_Pos:29_homolog:P1.1")
-    exclude.haplo <- list("Trait:FM07_LG:5_Pos:26_homolog:P1.4")
+    input.haplo <- list("Trait:PY06_LG:5_Pos:29_homolog:P1.1", "Trait:PY06_LG:5_Pos:29_homolog:P1.3")
+    exclude.haplo <- list("Trait:PY06_LG:5_Pos:29_homolog:P2.1", "Trait:PY06_LG:5_Pos:29_homolog:P2.4")
     
     p1.list <- select_haplo(input.haplo = input.haplo,
-                            exclude.haplo = exclude.haplo, 
-                            probs = viewqtl_qtlpoly$probs, 
-                            selected_mks = viewqtl_qtlpoly$selected_mks,
-                            effects.data = p)
+                                       exclude.haplo = exclude.haplo, 
+                                       probs = viewqtl_qtlpoly$probs, 
+                                       selected_mks = viewqtl_qtlpoly$selected_mks,
+                                       effects.data = p)
+    
     p1 <- p1.list[[1]]
     
     expect_equal(sum(p1[[1]]$data$probability), 431.9998, tolerance = 0.0001)
